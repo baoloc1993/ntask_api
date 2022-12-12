968e38bbdc76d22b0c82e5872b3154c7a7e0de1b
baoloc1993@gmail.com <baoloc1993@gmail.com>
2022-12-12 23:50:16 +0700

    fix bug

--- src/main/java/io/github/ntask_api/repository/NotificationTaskRepository.java
+++ src/main/java/io/github/ntask_api/repository/NotificationTaskRepository.java
@@ -14,7 +14,7 @@ public interface NotificationTaskRepository  extends JpaRepository<NotificationT
     @Query("select n from NotificationTask  n where n.task.id = ?1")
     public List<NotificationTask> findByTaskId(Long taskId);
 
-    @Query("select n from NotificationTask  n where n.user.id = ?1 and deleted_noti = 0")
+    @Query("select n from NotificationTask  n where n.user.id = ?1 and n.deleted_noti = false ")
     public Page<NotificationTask> findByUserId(Long userId, Pageable pageable);
 
     @Query("select n from NotificationTask  n where n.user.id = ?1")
